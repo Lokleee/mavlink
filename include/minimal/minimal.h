155d858ad7c494461e85771827f26b71f4b0d4ad
Lokleee <2152210748@qq.com>
2024-09-11 10:59:50 +0800

    add new message set_omni_attitude_target and omni_attitude_target to publish 3D thrust and attitude at the same time

--- include/minimal/minimal.h
+++ include/minimal/minimal.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_MINIMAL.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_MINIMAL_XML_HASH 5896972451676398998
+#define MAVLINK_MINIMAL_XML_HASH -2173610864318803917
 
 #ifdef __cplusplus
 extern "C" {
