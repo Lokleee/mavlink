7bd2a733fffe9520f0ba417b3c7fb0f116fa4f3b
Lokleee <2152210748@qq.com>
2024-10-15 16:08:20 +0800

    add new topic hover_thrust_estimate

--- include/minimal/minimal.h
+++ include/minimal/minimal.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_MINIMAL.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_MINIMAL_XML_HASH -2173610864318803917
+#define MAVLINK_MINIMAL_XML_HASH 2935604405374679795
 
 #ifdef __cplusplus
 extern "C" {
